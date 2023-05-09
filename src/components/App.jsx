c1183ecdae92f236919abcacd271aaf8cbdba6ba
Roman Kolobych <romankol1223@gmail.com>
2023-05-09 13:01:03 +0300

    Start work
    Start work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+
+import Counter from './Counter/Counter';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +11,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Counter />
     </div>
   );
 };
